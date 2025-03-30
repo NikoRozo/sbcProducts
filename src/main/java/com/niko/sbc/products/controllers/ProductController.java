fd3d6a5c3540cf81a46070678d67184656c8699c
Nicolás E Rozo E <niko.rozo92@outlook.com>
2025-03-29 21:36:05 -0500

    add ProductController with endpoints to retrieve all products and find by ID

---
+++ src/main/java/com/niko/sbc/products/controllers/ProductController.java
@@ -0,0 +1,31 @@
+package com.niko.sbc.products.controllers;
+
+import com.niko.sbc.products.entities.Product;
+import com.niko.sbc.products.services.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+    final private ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll() {
+        return ResponseEntity.ok(this.service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id) {
+        return service.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}

20461c60b05d63e7846bba64c650c7d8f4fe932d
Nicolás E Rozo E <niko.rozo92@outlook.com>
2025-03-29 20:32:04 -0500

    initialize Spring Boot application with Maven wrapper and basic configuration

---
+++ src/main/java/com/niko/sbc/products/SbcProductsApplication.java
@@ -0,0 +1,13 @@
+package com.niko.sbc.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbcProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbcProductsApplication.class, args);
+    }
+
+}
